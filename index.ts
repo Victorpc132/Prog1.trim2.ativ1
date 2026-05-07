fe1ab06983dd5abca2f791aa5b0cc964a9bef3f5
Your Name <you@example.com>
2026-05-07 11:30:08 -0300

    index ts ajustado

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { file } from "bun";
+
+const server = Bun.serve({
+  routes: {},
+
+  async fetch(req) {
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    const filePath = pathname === "/" 
+      ? "./public/index.html" 
+      : `./public${pathname}`;
+    const file = Bun.file(filePath);
+    const exists = await file.exists();
+    if (!exists) 
+      return new Response("Not Found", { status: 404 });
+    return new Response(file);
+  },
+});
+
+console.log(`Server running at ${server.url}`);
